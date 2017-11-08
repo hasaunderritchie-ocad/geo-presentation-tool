ba594599803a4dffa608bd4de164a611d2e6531a
Hugh <hritchie@faculty.ocadu.ca>
2017-11-08 15:36:36 -0500

    beginning work on controller

--- mapping-controller/index.js
+++ mapping-controller/index.js
@@ -4,19 +4,13 @@ const app = angular.module('myApp', ['ngAria', 'ngAnimate', 'ngMessages', 'ngRou
 
 // This is for the router. I need to modify this for this project.
 
-// app.config(function($routeProvider) {
-//   $routeProvider
-//    .when('/feed', {
-//     templateUrl: 'components/feed/feed.html',
-//     controller: 'feedController'
-//   }).when('/profile', {
-//     templateUrl: 'components/profile/profile.html',
-//     controller: 'profileController'
-//   }).when('/feed', {
-//     templateUrl: 'components/feed/feed.html',
-//     controller: 'feedController'
-//   }).when('/chat', {
-//     templateUrl: 'components/chat/chat.html',
-//     controller: 'chatController'
-//   }).otherwise({redirectTo:'/feed'})
-// });
+app.config(function($routeProvider) {
+  $routeProvider
+   .when('/', {
+    templateUrl: 'modules/home/home.html',
+    controller: 'homeController'
+  }).when('/session', {
+    templateUrl: 'modules/session/session.html',
+    controller: 'sessionController'
+  }).otherwise({redirectTo:'/'})
+});
--- mapping-controller/modules/d3.module.js
+++
@@ -1,5 +0,0 @@
-app.factory("d3", function(){
-  return function(){
-    console.log("some d3 stuff might go here.")
-  }
-})
