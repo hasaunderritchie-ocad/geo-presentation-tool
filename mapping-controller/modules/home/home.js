6d23dbf79389445bd9d438a400c90eff1395f10f
Hugh <hritchie@faculty.ocadu.ca>
2017-11-14 18:43:56 -0500

    last changes before presentation

--- mapping-controller/modules/home/home.js
+++ mapping-controller/modules/home/home.js
@@ -1,4 +1,4 @@
 app.controller('homeController', function($scope){
-  $scope.newSession = "myNewSession";
+
   // my controller
 })
--- mapping-controller/modules/session/session.js
+++ mapping-controller/modules/session/session.js
@@ -1,5 +1,15 @@
 app.controller('sessionController', function($scope, $window, mapping, data, pubnub){
   // my session
+    $scope.newSession = "testChannel";
+  $scope.scanning = false;
+  $scope.startScan = function(){
+    console.log("start scan with "+ $scope.scanning);
+    if(!$scope.scanning){
+        $scope.scanning = true;
+    } else {
+        $scope.scanning = false;
+    }
+  }
   session = 'testChannel';
   pubnub.connect(session);
   mapping.mapFunction('mapview', (response)=>{
